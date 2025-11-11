34b2dbd1ee78d3639cf7deb7901071f28a861e0b
melluche <derridjmellyna@gmail.com>
2025-11-11 10:53:25 +0100

    Create TD2.h

---
+++ MicroC/TD/TD2/TD2.h
@@ -0,0 +1,14 @@
+
+#define ARRAY_SIZE 3
+
+#include <stdio.h>
+
+void quizz(void);
+void setup2(void);
+void setup3(void);
+void fonction (int);
+void another_function(void);
+void stupid_fonction(int);
+void not_so_stupid_function(int* );
+void fillArray( int* array, int size);
+int getMeanValue(int* array, int size);
