a8c1493f40b4ec3b51f962af6d02067fcd1ae596
melluche <derridjmellyna@gmail.com>
2025-11-11 10:59:55 +0100

    Create td5.c

---
+++ MicroC/TD/TD5/td5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stm32l4xx_hal.h>
+#include <stdlib.h>
+#include "TD5.h"
+
+
+
+
+
+
+void setup_TD5(){
+
+}
+void loop_TD5(){
+	void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+
+
+}
