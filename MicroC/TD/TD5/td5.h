7c31fa7c785427331cef532c93f052d5375a77a8
melluche <derridjmellyna@gmail.com>
2025-11-11 11:00:15 +0100

    Create td5.h

---
+++ MicroC/TD/TD5/td5.h
@@ -0,0 +1,23 @@
+/*
+ * TD5.h
+ *
+ *  Created on: Oct 29, 2025
+ *      Author: 33658
+ */
+
+#ifndef INC_TD5_H_
+#define INC_TD5_H_
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+void setup_TD5();
+void loop_TD5();
+
+
+
+#endif /* INC_TD5_H_ */
